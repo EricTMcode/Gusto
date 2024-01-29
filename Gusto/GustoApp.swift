a0a8abf124717fc1fd1467cda0a81b036ad41b9a
Eric <ebagnaro@gmail.com>
2024-01-29 13:20:04 +0100

    Initial Commit

---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Eric on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
